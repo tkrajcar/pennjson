a4a038246b282fc487be5ff4b7665faf2209a4a8
Tim Krajcar <allegro@conmolto.org>
2013-07-25 16:45:34 -0700

    1.8.5p0

--- hdrs/svninfo.h
+++ hdrs/svninfo.h
@@ -1,3 +1,3 @@
-#define SVNREVISION "$Rev: 1181 $"
-#define SVNDATE "$Date: 2011-09-25 12:01:53 -0700 (Sun, 25 Sep 2011) $"
-/* Built at 20111105062154 */
+#define SVNREVISION "$Rev$"
+#define SVNDATE "$Date$"
+/* Built at 20120211224854 */
